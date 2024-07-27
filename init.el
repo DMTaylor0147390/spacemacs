d86d9c0b0fce394f692097b95be84914fc8b4b98
Derek <derekmtaylor1998@gmail.com>
2024-07-27 16:50:01 -0400

    Add Evil mode config

--- init.el
+++ init.el
@@ -106,6 +106,20 @@
   :ensure t
   :init (pdf-tools-install))
 
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
 (setq org-agenda-files (directory-files-recursively "~/org/tdls" "\\.org"))
 
 (defun get-target-path (buffer-path)
