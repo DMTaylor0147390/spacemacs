18ec21f78a31288ea4306ea861541729e8e6fdc6
Derek <derekmtaylor1998@gmail.com>
2025-09-29 22:50:00 -0400

    Restore init ELisp file after updating Spacemacs

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
 ;; Increase garbage collection threshold to speed up startup.
 (defconst emacs-start-time (current-time))
 (setq gc-cons-threshold 402653184 gc-cons-percentage 0.6)
-
+(setq byte-compile-warnings nil)
 ;; ---------------------------------------------------------------------------
 ;; * Load Core Paths
 ;; ---------------------------------------------------------------------------
@@ -93,3 +93,148 @@
     (unless (or (daemonp) (server-running-p))
       (message "Starting a server...")
       (server-start))))
+
+(setq evil-want-keybinding nil)
+
+(use-package exec-path-from-shell
+  :ensure t
+  :config (exec-path-from-shell-initialize))
+
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration t) ;; This is optional since it's already set to t by default.
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init)
+  (evil-collection-init 'evil-magit))
+
+(use-package nov
+  :after evil-collection
+  :ensure t
+  :config
+  (evil-collection-init 'evil-nov)
+  (evil-collection-nov-setup)
+  (evil-normal-state)
+  (evil-set-initial-state 'nov-mode 'normal))
+
+(use-package vterm
+  :ensure t
+  :after evil-collection
+  :init (evil-collection-vterm-setup)
+  :custom (vterm-always-compile-module t))
+
+(use-package flycheck
+  :ensure t)
+
+(use-package flycheck-clojure
+  :ensure t
+  :init (flycheck-clojure-setup))
+
+(use-package flycheck-clj-kondo
+  :ensure t)
+
+(use-package sly
+  :ensure t)
+
+(use-package slime
+  :ensure t)
+
+(use-package pdf-tools
+  :ensure t
+  :after evil-collection
+  :init
+  (pdf-tools-install)
+  (evil-collection-init 'evil-pdf)
+  (evil-collection-pdf-setup)
+  (evil-normal-state)
+  (evil-set-initial-state 'pdf-view-mode 'normal))
+
+(use-package org-zettelkasten
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook #'org-zettelkasten-mode)
+  (setq org-zettelkasten-directory (expand-file-name "~/org/org-zettelkasten"))
+  (setq org-zettelkasten-mapping-file (expand-file-name "~/org/org-zettelkasten/org-zettelkasten-index")))
+
+(use-package alchemist
+  :after evil-collection
+  :ensure t
+  :config
+  (evil-collection-alchemist-setup)
+  (evil-collection-init 'evil-alchemist)
+  (evil-normal-state)
+  (evil-set-initial-state 'alchemist-mode 'normal))
+
+(defun set-org-agenda-files ()
+  (setq org-agenda-files
+        (cl-remove-if
+         (lambda (s) (cl-search "sync-conflict" s))
+         (append (directory-files "~/org/" 1 ".org$")
+                 (directory-files-recursively "~/org/work" ".org$")
+                 (directory-files-recursively "~/org/tdls" ".org$")))))
+
+(defun get-target-path (buffer-path)
+  (replace-regexp-in-string "/org/" "/Google Drive/My Drive/org/" buffer-path nil 'literal))
+
+(defun sync-org-file-to-gdrive (org-file-path)
+  (copy-file org-file-path (get-target-path org-file-path) ""))
+
+(defun internet-up-p (&optional host)
+  (= 0 (call-process "ping" nil nil nil "-c" "1" "-W" "1"
+                     (if host host "www.google.com"))))
+
+(defun get-plain-text-file-path-for-org (org-file-path)
+  (replace-regexp-in-string "\\.org" ".txt" (buffer-file-name) nil 'literal))
+
+
+(defun org-sync-hook ()
+  (when (and (buffer-file-name) (string-match-p "/org/" (buffer-file-name)))
+    (progn
+      (set-org-agenda-files)
+      ;; (sync-org-file-to-gdrive (buffer-file-name))
+      (when (eq major-mode 'org-mode)
+        (org-ascii-export-to-ascii)
+        ;; (sync-org-file-to-gdrive (get-plain-text-file-path-for-org (buffer-file-name)))
+        )
+      )))
+
+(add-hook 'after-save-hook 'org-sync-hook)
+(setq create-lockfiles nil)
+
+(setq initial-scratch-message
+      ";; There is no final victory,\n;; as there is no final defeat.\n;; There is just the same battle.\n;; To be fought, over and over again.\n;; So toughen up, bloody toughen up.\n")
+
+(setq initial-major-mode 'org-mode)
+(add-to-list 'auto-mode-alist '("\\.epub\\'" . nov-mode))
+
+
+(evil-collection-init 'evil-nov)
+(evil-collection-nov-setup)
+(evil-collection-alchemist-setup)
+(add-hook 'nov-mode-hook 'evil-collection-init 100)
+(add-hook 'nov-mode-hook (lambda () (evil-collection-init 'evil-nov)) 100)
+(add-hook 'nov-mode-hook 'evil-collection-nov-setup 100)
+(add-hook 'nov-mode-hook (lambda () (evil-mode t)) 100)
+(add-hook 'nov-mode-hook 'evil-normal-state 100)
+
+(evil-normal-state)
+(add-hook 'pdf-view-mode-hook 'evil-collection-init 100)
+(add-hook 'pdf-view-mode-hook (lambda () (evil-collection-init 'evil-pdf)) 100)
+(add-hook 'pdf-view-mode-hook 'evil-collection-nov-setup 100)
+(add-hook 'pdf-view-mode-hook (lambda () (evil-mode t)) 100)
+(add-hook 'pdf-view-mode-hook 'evil-normal-state 100)
+
+(add-hook 'alchemist-mode-hook 'evil-collection-init 100)
+(add-hook 'alchemist-mode-hook (lambda () (evil-collection-init 'evil-alchemist)) 100)
+(add-hook 'alchemist-mode-hook 'evil-collection-alchemist-setup 100)
+(add-hook 'alchemist-mode-hook (lambda () (evil-mode t)) 100)
+(add-hook 'alchemist-mode-hook 'evil-normal-state 100)
+
+(setq auth-sources '("~/.authinfo"))
